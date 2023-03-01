98b1747e24769aaf55a7a11cee048226f71b7a65
Harsha <mharshithsatyakrishna@gmail.com>
2023-03-01 19:22:03 +0530

    Commit-3

--- client/src/pages/UserProfile/EditProfileForm.jsx
+++ client/src/pages/UserProfile/EditProfileForm.jsx
@@ -1,10 +1,23 @@
 import React, {useState} from 'react'
+import { useDispatch } from "react-redux";
+import { updateProfile } from "../../actions/users";
 
 const EditProfileForm = ({currentUser , setSwitch}) => {
 
 const [name, setName] = useState(currentUser?.result?.name);
 const [about, setAbout] = useState(currentUser?.result?.about);
 const [tags, setTags] = useState([]);
+const dispatch = useDispatch();
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (tags[0] === "" || tags.length === 0) {
+        dispatch(updateProfile(currentUser?.result?._id, { name, about, tags:currentUser?.result.tags }));
+    } else {
+      dispatch(updateProfile(currentUser?.result?._id, { name, about, tags }));
+    }
+    setSwitch(false);
+  };
 
   return (
     <div>
@@ -14,7 +27,7 @@ const [tags, setTags] = useState([]);
         <h2 className="edit-profile-title-2">
         Public information
         </h2>
-        <form className="edit-profile-form">
+        <form className="edit-profile-form" onSubmit={handleSubmit}>
             <label htmlFor="name">
                 <h3>Display name</h3>
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
